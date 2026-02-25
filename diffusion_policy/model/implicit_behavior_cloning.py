c26494c615e9b4d883fa3bf97243b6eb5063246a
Toby Buckley <buckley.toby@gmail.com>
2026-02-24 22:35:42 -0600

    use a common implicit policy

--- diffusion_policy/model/implicit_behavior_cloning.py
+++ diffusion_policy/model/implicit_behavior_cloning.py
@@ -63,54 +63,14 @@ from tqdm import (
     tqdm
 )
 
-class ImplicitBehaviorCloningPolicy(nn.Module):
+from dexnex_intelligence.all import (
+    ImplicitPolicy,
+    ImplicitAlgorithm
+)
+
+class ImplicitBehaviorCloningAlgorithm(ImplicitAlgorithm):
     def __init__(self,
-            action_shape: dict,
-            obs_encoder_maker: ObsEncoderMaker,
-            n_obs_steps, # input time-length
-            down_dims=(256,512,1024),
-            kernel_size=5,
-            n_groups=8,
-            cond_predict_scale=True,
-            num_mid_module_repeats = 4,
-            ):
-        super().__init__()
-        
-        self.obs_encoder_maker = obs_encoder_maker
-
-        # my members
-        self.obs_encoder = self.obs_encoder_maker.get()
-
-        # parse shape_meta
-        assert len(action_shape) == 1
-        self.action_dim = action_shape[0]
-        obs_feature_dim = self.obs_encoder.output_shape()[0]
-        global_cond_dim = obs_feature_dim * n_obs_steps
-
-        self.model = ConditionalSlashnet1D(
-                        input_dim=self.action_dim,
-                        global_cond_dim=global_cond_dim,
-                        down_dims=down_dims,
-                        kernel_size=kernel_size,
-                        n_groups=n_groups,
-                        cond_predict_scale=cond_predict_scale,
-                        num_mid_module_repeats=num_mid_module_repeats,
-                    )
-        
-        
-
-        print_nb_params(self.model, "Diffusion params")
-        print_nb_params(self.obs_encoder, "Vision params")
-
-    def forward_obs_encoder(self, *args, **kwargs):
-        return self.obs_encoder(*args, **kwargs)
-
-    def forward(self, *args, **kwargs):
-        return self.model(*args, **kwargs)
-
-class ImplicitBehaviorCloningAlgorithm(BaseImagePolicy):
-    def __init__(self,
-            policy: ImplicitBehaviorCloningPolicy,
+            policy: ImplicitPolicy,
             num_inference_steps = 8,
             step_size = 1e-1,
     ):
@@ -126,82 +86,6 @@ class ImplicitBehaviorCloningAlgorithm(BaseImagePolicy):
         # save from global config
         self.action_rel_indices = globals.CONFIG.action_rel_indices #type:ignore
         self.horizon = len(self.action_rel_indices)
-        
-    def per_sample_backward(self, btrajectory, timestep, bglobal_cond):
-        def fcn(trajectory, global_cond):
-            return self.policy(trajectory.unsqueeze(0), timestep, global_cond = global_cond.unsqueeze(0)).squeeze()
-        
-        # 2. Vectorize the gradient calculation across the batch dimension
-        # (0, 0) tells vmap to map over the 0th dimension of x and y
-        vmap_fcn = vmap(grad(fcn), in_dims=(0, 0))
-        per_sample_grads = vmap_fcn(btrajectory, bglobal_cond)
-        
-        assert(per_sample_grads.shape == btrajectory.shape)
-        return per_sample_grads
-
-    # alias
-    def predict_action(self, nobs): #type:ignore
-        return self.inference(nobs)
-    
-    # alias
-    def infer(self, nobs_dict: dict, task_id=None, noise_scheduler=None):
-        # return 3 things for backwards compat
-        action = self.inference(nobs_dict)
-        return action, None, action
-    
-    @torch.enable_grad()
-    def inference(self, nobs: dict):
-        To = 1
-        value = next(iter(nobs.values()))
-        B = value.shape[0] # batch
-        T = self.horizon # trajectory length
-        Da = self.policy.action_dim # action dimension
-        device = self.device
-        dtype = self.dtype
-        step_size = self.step_size # might come from scheduler instead
-        timestep = 0.0
-
-        # reshape obs: B, T, ... to B*T ...
-        this_nobs = dict_apply(nobs, 
-            lambda x: x[:,-To:,...].reshape(-1,*x.shape[2:]))
-
-        # get encoded obs
-        nobs_features = self.policy.forward_obs_encoder(this_nobs)
-        global_cond = nobs_features.reshape(B, -1)
-
-        # dummy trajectory
-        dummy_trajectory = torch.zeros(size=(B, T, Da), device=device, dtype=dtype)
-
-        # randomly initialize a
-        trajectory = self.make_noise(dummy_trajectory)
-        trajectory.requires_grad = True
-
-        # do gradient ascent
-        for _ in range(self.num_inference_steps):
-        # for _ in tqdm(range(self.num_inference_steps), desc="Inference steps"):
-            # forward & backward
-            a_grad = self.per_sample_backward(trajectory, timestep, global_cond)
-
-            # grad-free update step, to make a new trajectory object
-            with torch.no_grad():
-                assert(a_grad.shape == trajectory.shape)
-                trajectory = trajectory + step_size * a_grad
-            
-        return trajectory
-
-        
-    def make_noise(self, x, generator=None):
-        """
-        straight from DiffusionModel
-        """
-        n = torch.randn(x.shape, dtype=x.dtype, device=x.device, generator=generator)
-        
-        must_clamp = globals.CONFIG.clamp #type:ignore
-        
-        if must_clamp:
-            n = torch.clamp(n, -1.0, 1.0)
-            
-        return n
 
     def make_alphas(self, trajectory):
         # batch size of the traj
@@ -248,7 +132,6 @@ class ImplicitBehaviorCloningAlgorithm(BaseImagePolicy):
         nactions = nbatch['action']
         batch_size = nactions.shape[0]
 
-        global_cond = None
         trajectory = nactions
         cond_data = trajectory
         timesteps = 0.0
@@ -266,13 +149,8 @@ class ImplicitBehaviorCloningAlgorithm(BaseImagePolicy):
         this_nobs = dict_apply(nobs, 
             lambda x: x[:,-To:,...].reshape(-1,*x.shape[2:]))
 
-        # get encoded obs
-        nobs_features = self.policy.forward_obs_encoder(this_nobs)
-        
-        global_cond = nobs_features.reshape(batch_size, -1)
-
         # predict the score
-        score = self.policy.forward(noisy_trajectory, timesteps, global_cond=global_cond)
+        score = super().forward(this_nobs, noisy_trajectory)
 
         # loss -- we want our score to match the alpha value
         loss = F.mse_loss(score.squeeze(), alpha.squeeze())
@@ -283,22 +161,3 @@ class ImplicitBehaviorCloningAlgorithm(BaseImagePolicy):
         # we're done
         return loss, None, timesteps
 
-
-    def get_val_action_mse_error(self, nbatch, task_id=None):
-        # ground truth action
-        nobs = nbatch['obs']
-        gt_action = nbatch['action']
-        
-        # denoise
-        ntrajectory = self.predict_action(nobs)
-        
-        # extract the predicted action
-        pred_action = ntrajectory
-        
-        # calc mse
-        mse = torch.nn.functional.mse_loss(pred_action, gt_action)
-        
-        # move to cpu
-        action_mse_error = mse.item()
-        
-        return action_mse_error
