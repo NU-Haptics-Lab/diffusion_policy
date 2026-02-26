faa962546bada8a22f7a75432c34beaf37d41634
Toby Buckley <buckley.toby@gmail.com>
2026-02-26 15:11:00 -0600

    none protection

--- diffusion_policy/model/bc_exploration_algorithm.py
+++ diffusion_policy/model/bc_exploration_algorithm.py
@@ -269,9 +269,10 @@ class BCExplorationAlgorithm(nn.Module):
         assert(action.shape[2] == NUM_ACTIONS) # action dim
         
         ### Logging
-        globals.LOGGER.log_one("schedulers/exploration_BC", self.exploration_BC.get_value(globals.STEP))
-        globals.LOGGER.log_one("schedulers/exploration_random", self.exploration_random.get_value(globals.STEP))
-        globals.LOGGER.log_one("schedulers/exploration_policy", self.exploration_policy.get_value(globals.STEP))
+        if globals.LOGGER is not None:
+            globals.LOGGER.log_one("schedulers/exploration_BC", self.exploration_BC.get_value(globals.STEP))
+            globals.LOGGER.log_one("schedulers/exploration_random", self.exploration_random.get_value(globals.STEP))
+            globals.LOGGER.log_one("schedulers/exploration_policy", self.exploration_policy.get_value(globals.STEP))
 
         ### End Logging
 
